21f0a9ce8b73ef71665a05ea14de49dc4de593f6
백종열 <geeksbaek@gmail.com>
2016-09-19 12:15:51 +0900

    add max concurrent flag

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 )
 
 var (
-	flagID = flag.String("id", "", "id")
-	flagPW = flag.String("pw", "", "password")
+	flagID     = flag.String("id", "", "id")
+	flagPW     = flag.String("pw", "", "password")
+	flagMaxCon = flag.Int("c", 10, "maximum concurrent request")
 )
 
 func main() {
@@ -25,7 +26,7 @@ func main() {
 
 	fmt.Println("모든 글과 댓글을 불러오는 중입니다. 잠시만 기다려주세요.")
 	start := time.Now()
-	data := s.FetchAll()
+	data := s.FetchAll(*flagMaxCon)
 
 	fmt.Printf("글 %v개, 댓글 %v개 ", len(data.As), len(data.Cs))
 	fmt.Println("불러오기를 완료하였습니다.")
@@ -33,7 +34,7 @@ func main() {
 
 	fmt.Println("삭제를 시작합니다. 잠시만 기다려주세요.")
 	middle := time.Now()
-	s.DeleteAll(data, func(i, n int) {
+	s.DeleteAll(*flagMaxCon, data, func(i, n int) {
 		fmt.Printf("\r삭제 중... %v/%v", i, n)
 	})
 	fmt.Println()
